d0cdd3133e738dc9efcc1cc3b46309b6292048a6
josh098741 <morongemokaya@gmail.com>
2025-10-15 13:49:18 +0300

    Additional commit

--- CHAT-AI/client/src/App.jsx
+++ CHAT-AI/client/src/App.jsx
@@ -1,7 +1,15 @@
-
+import React from 'react'
+import Sidebar from './components/Sidebar'
 
 function App() {
   
+    return(
+        <>
+            <div>
+                <Sidebar />
+            </div>
+        </>
+    );
 }
 
 export default App
---
+++ CHAT-AI/client/src/components/Chatbox.jsx
@@ -0,0 +1,9 @@
+
+
+function Chatbox(){
+    return(
+        <div></div>
+    )
+}
+
+export default Chatbox
